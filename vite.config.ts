c28809b5153765ae4471bb87eda4e65105244e20
Ino4811 <inoryu4811@gmail.com>
2025-02-25 04:51:03 +0900

    feat: bind 0.0.0.0 to access from iPad

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,18 @@ const entry = './app/server.ts'
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
     return {
+      server: {
+        host: true,
+        port: 5174,
+      },
       plugins: [client()],
     }
   } else {
     return {
+      server: {
+        host: true,
+        port: 5174,
+      },
       build: {
         emptyOutDir: false,
       },
